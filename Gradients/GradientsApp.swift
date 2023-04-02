2210562a04c5e5058560737a146fa9327142895c
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-04-02 21:40:49 +0600

    project setup

---
+++ Gradients/GradientsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GradientsApp.swift
+//  Gradients
+//
+//  Created by Shah Md Imran Hossain on 2/4/23.
+//
+
+import SwiftUI
+
+@main
+struct GradientsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
